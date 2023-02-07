6ebbee79a8f5c036b3fb9c078f373a14ad8ed64e
Bogdan Nazarchuk <111483751+Virtuozio@users.noreply.github.com>
2023-02-07 20:52:45 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,41 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onClick);
+function onClick(event) {
+  event.preventDefault();
+  let firstDelay = Number(form.elements.delay.value);
+  let step = Number(form.elements.step.value);
+  let amount = Number(form.elements.amount.value);
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, (firstDelay += step))
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+          timeout: 3000,
+        });
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+          timeout: 3000,
+        });
+      });
   }
+}
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      const data = {
+        position: position,
+        delay: delay,
+      };
+      if (shouldResolve) {
+        resolve(data);
+      } else {
+        reject(data);
+      }
+    }, delay);
+  });
 }
